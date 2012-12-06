fdfbb080a3bb3f326f3dc69c2f380b2bad4f2fd5
403264268@qq.com <403264268@qq.com@bf4ff70b-eeb6-e956-9098-efb552d8008d>
2012-12-06 13:11:13 +0000

    淘宝订单获取

---
+++ trunk/check/src/com/check/util/top/TopCometMessageListenerImpl.java
@@ -0,0 +1,62 @@
+package com.check.util.top;
+
+import com.taobao.api.domain.NotifyTrade;
+import com.taobao.api.internal.stream.message.TopCometMessageListener;
+
+public class TopCometMessageListenerImpl implements TopCometMessageListener {
+
+	private TopApiService topApiService;
+
+	public TopCometMessageListenerImpl(TopApiService topApiService) {
+		this.topApiService = topApiService;
+	}
+
+	public void onConnectMsg(String message) {
+	}
+
+	public void onHeartBeat() {
+	}
+
+	public void onReceiveMsg(String message) {
+		try {
+			Object obj = MessageDecode.decodeMsg(message);
+			if (obj instanceof NotifyTopats) { // 异步任务
+				System.out.println(message);
+				NotifyTopats nt = (NotifyTopats) obj;
+				if ("done".equals(nt.getTaskStatus())) {
+					String url = topApiService.getTaskResultUrl(nt.getTaskId());
+					if (url != null) {
+						topApiService.downloadAndProcess(url);
+					}
+				}
+			} else if (obj instanceof NotifyTrade) { // 交易消息
+				NotifyTrade nt = (NotifyTrade) obj;
+				System.out.println(nt.getSellerNick() + ":" + nt.getTid()); // FIXME 获取订单详情并保存到数据库
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void onDiscardMsg(String message) {
+	}
+
+	public void onServerUpgrade(String message) {
+	}
+
+	public void onServerRehash() {
+	}
+
+	public void onServerKickOff() {
+	}
+
+	public void onClientKickOff() {
+	}
+
+	public void onOtherMsg(String message) {
+	}
+
+	public void onException(Exception e) {
+	}
+
+}
